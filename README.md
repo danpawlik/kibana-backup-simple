b53307411ea416874cf4e788b2b0337bc85e4d35
Daniel Pawlik <pawlik.dan@gmail.com>
2021-03-16 12:02:42 +0100

    Fix backup and restore procedure; updated README file

--- README.md
+++ README.md
@@ -3,19 +3,41 @@ Simple backup for Kibana saved objects: config, index patterns, dashboards, save
 Uses python3 and [python-requests](https://requests.readthedocs.io/) library, which you have to install on your every host anyway, because it's awesome.
 
 ### Usage
+Available actions:
 
-`kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] backup > backup.ndjson`
+* `backup` - write backup file in newline-delimitered json format to stdout
+* `restore` - restore backup from stdin
+* `convert` - convert `json` or `ndjson` format to `yaml`
 
-`cat backup.ndjson | kibana-backup.py [--kibana-url KIBANA_URL] [--user USER] [--password PASSWORD] restore`
+Available arguments:
 
-* `backup` write backup file in newline-delimitered json format to stdout
-* `restore` restore backup from stdin
-* `--kibana-url` base URL to access Kibana API, default: `http://127.0.0.1:5601`
-* `--space-id` Kibana space id. If not set then the default space is used.
-* `--user` Kibana user
-* `--password` Kibana password
+* `--kibana-url` - base URL to access Kibana API, default: `http://127.0.0.1:5601`
+* `--space-id` - kibana space id. If not set then the default space is used.
+* `--user` - kibana user
+* `--password` - kibana password
+* `--file` - file that will be converted or restored
+* `--backup-dir` - backup location
+* `--extension` - backup extension. Available `yml` or `yaml` or `json`. Defaults to `json`
 
- *Note:* To use the default space you should not set `--space-id` parameter. Setting it to the default space id: `default` does not work.
+*Note:* To use the default space you should not set `--space-id` parameter. Setting it to the default space id: `default` does not work.
+
+Example:
+
+Below examples has been done on Kibana Opendistro for Elasticsearch.
+
+```
+# Backup as json
+kibana-backup.py --kibana-url https://$(hostname):5601 --backup-dir $(pwd) --user admin --password mypassword  backup
+
+# Backup as a yaml
+kibana-backup.py --kibana-url https://$(hostname):5601 --extension yml --backup-dir $(pwd) --user admin --password mypassword  backup
+
+# Restore backup
+kibana-backup.py --kibana-url https://$(hostname):5601 --file dashboard.yml --user admin --password mypassword restore
+
+# Convert json/ndjson to yaml
+kibana-backup.py --file dashboard.ndjson --extension yaml convert
+```
 
 ### Documentation
 
--- kibana-backup.py
+++ kibana-backup.py
@@ -22,23 +22,122 @@ import yaml
 # \"type\" must be one of:
 # [config, map, canvas-workpad, canvas-element, index-pattern,
 #  visualization, search, dashboard, url]
-saved_objects_types = (
-    'config', 'map', 'canvas-workpad', 'canvas-element', 'index-pattern',
-    'visualization', 'search', 'dashboard', 'url')
+saved_objects_types = ('config', 'map', 'canvas-workpad', 'canvas-element',
+                       'index-pattern', 'visualization', 'search', 'dashboard',
+                       'url')
 
 to_remove_keys = ['updated_at', 'version', 'migrationVersion']
 
 
-def convert_to_yaml(text):
-    text = remove_reference(text)
+def get_arguments():
+    args_parser = argparse.ArgumentParser(
+        description='Backup and restore Kibana saved objects. '
+        'Writes backup to stdout and reads from stdin.')
+    args_parser.add_argument('action',
+                             choices=['backup', 'restore', 'convert'])
+    args_parser.add_argument('--kibana-url',
+                             default='http://127.0.0.1:5601',
+                             help='URL to access Kibana API')
+    args_parser.add_argument('--space-id',
+                             default='',
+                             help='Kibana space id. If not set then the '
+                             'default space is used.')
+    args_parser.add_argument('--user', default='', help='Kibana user')
+    args_parser.add_argument('--password', default='', help='Kibana password')
+    args_parser.add_argument('--backup-dir',
+                             help='Dir where backups will be stored')
+    args_parser.add_argument('--file', help='File to restore or convert')
+    args_parser.add_argument('--resolve-conflicts',
+                             default=True,
+                             help='Resolve conflicts by removing index '
+                             'id reference in backup file')
+    args_parser.add_argument('--insecure',
+                             action='store_true',
+                             help='Use that option to ignore if SSL cert '
+                             'has been verified by root CA')
+    args_parser.add_argument('--extension',
+                             default='ndjson',
+                             help='Backup extension type')
+    return args_parser.parse_args()
+
+
+def convert_to_yaml(text, remove_references):
+    if remove_references:
+        text = remove_reference(text)
     return yaml.dump(json.loads(text))
 
 
-def save_content_to_file(text, backup_file, extension):
+def save_content_to_file(text, backup_file, extension, remove_references=True):
     if extension in ['yaml', 'yml']:
-        text = convert_to_yaml(text)
+        text = convert_to_yaml(text, remove_references)
     with open(backup_file, 'a') as f:
         f.write(text)
+
+
+def check_if_empty(text):
+    text = json.loads(text)
+    if 'exportedCount' in text and text['exportedCount'] == 0:
+        return True
+
+
+def remove_obj_keys(ref):
+    for k in to_remove_keys:
+        ref.pop(k, None)
+    return ref
+
+
+def remove_reference(text):
+    new_text = []
+    new_references = []
+    try:
+        text = json.loads(text)
+        for ref in text['references']:
+            if not ref.get('id').startswith('AX') and len(ref.get('id')) != 20:
+                new_references.append(remove_obj_keys(ref))
+        text['references'] = new_references
+    except json.decoder.JSONDecodeError:
+        text = ndjson.loads(text)
+        for ref in text:
+            if (not ref['references'][0].get('id').startswith('AX')
+                    and len(ref['references'][0].get('id')) != 20):
+                new_text.append(remove_obj_keys(ref))
+
+    return json.dumps(new_text) if new_text else json.dumps(text)
+
+
+def make_request(url, user, password, text, insecure=False, retry=True):
+    r = None
+    try:
+        r = requests.post(url,
+                          auth=(user, password),
+                          headers={'kbn-xsrf': 'reporting'},
+                          files={'file': ('backup.ndjson', text)},
+                          timeout=10,
+                          verify=insecure)
+    except requests.exceptions.ReadTimeout:
+        if not retry:
+            print("Importing failed. Retrying...")
+            time.sleep(10)
+            make_request(url, user, password, text, insecure)
+
+    if r and "Please enter your credentials" in r.text:
+        print("Please provide correct username and password")
+        sys.exit(1)
+
+    return r
+
+
+def _get_file_content(backup_file, extension=None):
+    if (args.file.endswith('yml')
+            or args.file.endswith('yaml')):
+        extension = 'yaml'
+        with open(args.file) as f:
+            text = yaml.safe_load(f)
+    else:
+        with open(args.file) as f:
+            text = f.readlines()
+        extension = 'json'
+    return text, extension
 
 
 def backup(kibana_url, space_id, user, password, backup_dir, insecure,
@@ -58,17 +157,15 @@ def backup(kibana_url, space_id, user, password, backup_dir, insecure,
         url = kibana_url + '/api/saved_objects/_export'
     for obj_type in saved_objects_types:
         print("Working on %s" % obj_type)
-        r = requests.post(
-            url,
-            auth=(user, password),
-            headers={
-                'Content-Type': 'application/json',
-                'kbn-xsrf': 'reporting'
-            },
-            data='{ "type": "' + obj_type +
-            '","excludeExportDetails": true}',
-            verify=insecure
-        )
+        r = requests.post(url,
+                          auth=(user, password),
+                          headers={
+                              'Content-Type': 'application/json',
+                              'kbn-xsrf': 'reporting'
+                          },
+                          data='{ "type": "' + obj_type +
+                          '","excludeExportDetails": true}',
+                          verify=insecure)
 
         if r.status_code == 400:
             # Print warning on missing object, but continue
@@ -111,13 +208,28 @@ def restore(kibana_url, space_id, user, password, text, resolve_conflicts,
     for kib_obj in text:
         print("Working on %s" % kib_obj)
 
-        kib_obj = json.dumps(kib_obj)
+        if not isinstance(kib_obj, dict):
+            # Ensure that the kib_obj is one-time converted json object
+            kib_obj = json.dumps(json.loads(kib_obj))
+        else:
+            kib_obj = json.dumps(kib_obj)
 
         if check_if_empty(kib_obj):
             print("Spotted empty object. Continue...")
             continue
 
         r = make_request(url, user, password, kib_obj, insecure)
+
+        if r.status_code == 401:
+            print("Unauthorized. Please provide user and password")
+
+        try:
+            response_error = json.loads(r.text)
+            if response_error['errors']:
+                print("\n\nSome problem on restoring %s: %s\n\n" %
+                      (kib_obj, response_error['errors']))
+        except Exception as e:
+            print("Kibana restore requests objects does not look correct")
 
         if not r:
             print("Can not import %s into Kibana" % kib_obj)
@@ -132,109 +244,37 @@ def restore(kibana_url, space_id, user, password, text, resolve_conflicts,
         r.raise_for_status()  # Raises stored HTTPError, if one occurred.
 
 
-def remove_reference(text):
-    new_text = []
-    new_references = []
-    try:
-        text = json.loads(text)
-        for ref in text['references']:
-            if not ref['id'].startswith('AX') and len(ref['id']) != 20:
-                new_references.append(remove_obj_keys(ref))
-        text['references'] = new_references
-    except json.decoder.JSONDecodeError:
-        text = ndjson.loads(text)
-        for ref in text:
-            if (not ref['references'][0]['id'].startswith('AX')
-                    and len(ref['references'][0]['id']) != 20):
-                new_text.append(remove_obj_keys(ref))
-
-    return json.dumps(new_text) if new_text else json.dumps(text)
-
-
-def remove_obj_keys(ref):
-    for k in to_remove_keys:
-        ref.pop(k, None)
-    return ref
-
-
-def make_request(url, user, password, text, insecure=False, retry=True):
-    r = None
-    try:
-        r = requests.post(
-            url,
-            auth=(user, password),
-            headers={'kbn-xsrf': 'reporting'},
-            files={'file': ('backup.ndjson', text)},
-            timeout=10,
-            verify=insecure
-        )
-    except requests.exceptions.ReadTimeout:
-        if not retry:
-            print("Importing failed. Retrying...")
-            time.sleep(10)
-            make_request(url, user, password, text, insecure)
-
-    if "Please enter your credentials" in r.text:
-        print("Please provide correct username and password")
-        sys.exit(1)
-
-    return r
-
-
-def check_if_empty(text):
-    text = json.loads(text)
-    if 'exportedCount' in text and text['exportedCount'] == 0:
-        return True
+def convert(text, extension, convert_file):
+    convert_file = "%s-converted.%s" % (convert_file, extension)
+    for kib_obj in text:
+        save_content_to_file(kib_obj, convert_file, extension, False)
 
 
 if __name__ == '__main__':
-    args_parser = argparse.ArgumentParser(
-        description='Backup and restore Kibana saved objects. '
-                    'Writes backup to stdout and reads from stdin.')
-    args_parser.add_argument('action', choices=['backup', 'restore'])
-    args_parser.add_argument('--kibana-url', default='http://127.0.0.1:5601',
-                             help='URL to access Kibana API')
-    args_parser.add_argument('--space-id', default='',
-                             help='Kibana space id. If not set then the '
-                                  'default space is used.')
-    args_parser.add_argument('--user', default='', help='Kibana user')
-    args_parser.add_argument('--password', default='', help='Kibana password')
-    args_parser.add_argument('--backup-dir',
-                             help='Dir where backups will be stored')
-    args_parser.add_argument('--restore-file', help='ndjson file to restore')
-    args_parser.add_argument('--resolve-conflicts', default=True,
-                             help='Resolve conflicts by removing index '
-                                  'id reference in backup file')
-    args_parser.add_argument('--insecure', action='store_true',
-                             help='Use that option to ignore if SSL cert '
-                                  'has been verified by root CA')
-    args_parser.add_argument('--extension', default='ndjson',
-                             help='Backup extension type')
-    args = args_parser.parse_args()
-
+    args = get_arguments()
     kibana_url = args.kibana_url
-    if (not args.kibana_url.startswith('http') and
-            not args.kibana_url.startswith('https')):
+    if (not args.kibana_url.startswith('http')
+            and not args.kibana_url.startswith('https')):
         kibana_url = "http://%s" % args.kibana_url
 
     if args.action == 'backup':
-        print(backup(kibana_url, args.space_id, args.user, args.password,
-                     args.backup_dir, args.insecure, args.extension)
-              )
+        backup(kibana_url, args.space_id, args.user, args.password,
+               args.backup_dir, args.insecure, args.extension)
 
     elif args.action == 'restore':
-        if args.restore_file:
-            if (args.restore_file.endswith('yml')
-                    or args.restore_file.endswith('yaml')):
-                extension = 'yaml'
-                with open(args.restore_file) as f:
-                    text = yaml.safe_load(f)
-            else:
-                with open(args.restore_file) as f:
-                    text = f.readlines()
-            extension = 'json'
+        if args.file:
+            text, extension = _get_file_content(args.file,
+                                                args.extension)
         else:
             text = ''.join(sys.stdin.readlines())
 
-        restore(kibana_url, args.space_id, args.user, args.password,
-                text, args.resolve_conflicts, args.insecure, extension)
+        restore(kibana_url, args.space_id, args.user, args.password, text,
+                args.resolve_conflicts, args.insecure, extension)
+    elif args.action == 'convert':
+        if args.file:
+            text, extension = _get_file_content(args.file,
+                                                args.extension)
+        else:
+            text = ''.join(sys.stdin.readlines())
+
+        convert(text, args.extension, args.file)
